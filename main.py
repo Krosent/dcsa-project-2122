47ce4140ca183d629bc7195f0ad770adeb1d879e
Krosent <krosent2@gmail.com>
2021-10-28 17:46:39 +0200

    Implementation of first two mapping steps

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from mrjob.job import MRJob
 import re
 
+from mrjob.step import MRStep
 
-class MRWordFrequencyCount(MRJob):
+class MRTopTenKeywordsForEachGenre(MRJob):
 
-    def mapper(self, _, line):
-        yield "chars", len(line)
-        yield "words", len(line.split())
-        yield "lines", 1
+    def steps(self):
+        return [MRStep(mapper=self.determine_titles_and_genres_mapper),
+                MRStep(mapper=self.transform_titles_and_genres_into_lists_mapper),
+                MRStep(mapper=self.titles_normalization_mapper)]
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+    def determine_titles_and_genres_mapper(self, _, line):
 
+        splitted_columns = line.split(',')
+        title = splitted_columns[1]
+        genres = splitted_columns[2]
+        yield "record", (title, genres)
+
+    def transform_titles_and_genres_into_lists_mapper(self, _, input_row):
+        yield "record", (input_row[0].split(' '), input_row[1].split('|'))
+
+    # clean titles
+    # title tags should not have numbers, prepositions, symbols
+    def titles_normalization_mapper(self, _, input_row):
+        keywords = input_row[0]
+        genres = input_row[1]
+
+        keywords = list(filter(str.isalpha, keywords))
+        yield "record", (keywords, genres)
+
+# example of bug: [["Tree"], [" 1951: A Portrait of James Dean (2012)\""]]
 
 if __name__ == '__main__':
-    MRWordFrequencyCount.run()
+    MRTopTenKeywordsForEachGenre.run()
