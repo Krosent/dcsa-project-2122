85c2cd2dc208a9c89e1b83b24a129d5a65e48787
Artyom Kuznetsov <artyom.kuznetsov@vub.be>
2021-11-21 22:26:33 +0100

    Task 2 implementation is done

--- task2.py
+++ task2.py
@@ -6,12 +6,23 @@ import re
 
 class MRReverseLinkGraph(MRJob):
     def steps(self):
-        return [MRStep(mapper=self.initial_mapper)]
+        return [MRStep(mapper=self.initial_mapper),
+                MRStep(mapper=self.reverse_pairs),
+                MRStep(reducer=self.target_reducer)
+                ]
 
+    # return any source, target
     def initial_mapper(self, _, line):
         if line[0] != '#':
             split_cols = re.split('\t', line)
             yield None, (split_cols[0], split_cols[1])
+
+    # transform to target, sources
+    def reverse_pairs(self, _, value):
+        yield value[1], value[0]
+
+    def target_reducer(self, target, sources):
+        yield target, list(sources)
 
 
 if __name__ == '__main__':
