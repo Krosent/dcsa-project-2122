0adc81387df90b7cf8003e2df175d3953406517a
Artyom Kuznetsov <artyom.kuznetsov@vub.be>
2021-12-06 21:42:36 +0100

    [Task 4] read_file_mapper and string_to_float_value_mapper implementation

--- task4.py
+++ task4.py
@@ -8,7 +8,15 @@ import math
 class MRFrobeniusNorm(MRJob):
 
     def steps(self):
-        return []
+        return [MRStep(mapper=self.read_file_mapper), MRStep(mapper=self.string_to_float_value_mapper)]
+
+    def read_file_mapper(self, _, line):
+        row = re.split(' ', line)
+        yield None, row
+
+    # transforms string values into float values
+    def string_to_float_value_mapper(self, _, row):
+        yield _, list(map(lambda col: float(col), row))
 
 
 if __name__ == '__main__':
